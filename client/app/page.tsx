b5e8ab7a7d0d37348a827ee8f24037c42ad6a02f
Manami <vaattii1996@gmail.com>
2023-12-01 18:20:15 -0800

    modify property card

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,10 +6,13 @@ import { Property } from "./types/types";
 
 export default async function Home() {
   const properties: Property[] = await getAllProperties();
+  // const properties: Property[] = [];
+
   return (
     <ClientOnly>
       <Container>
-        <main className="relative h-full top-20 ml-0 md:ml-56 overflow-y-auto">
+        {/* <main className="relative h-full top-20 ml-0 md:ml-56 overflow-y-auto"> */}
+        <main className="relative h-full top-20">
           <PropertiesList properties={properties} />
         </main>
       </Container>
