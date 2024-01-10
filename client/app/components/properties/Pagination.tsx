4d6a7d03830cfa589b93f103bf5194199148bd43
小針純 <kobarijun@kobarijunnoMacBook-Pro.local>
2024-01-10 15:05:28 -0800

    edit pagination components

--- client/app/components/properties/Pagination.tsx
+++ client/app/components/properties/Pagination.tsx
@@ -11,6 +11,8 @@ type Props = {
     num_pages: number;
 }
 
+const range = (start: number, end: number) => [...Array(end - start + 1)].map((_, i) => start + i);
+
 export default function Pagination({
     total,
     current_page,
@@ -49,42 +51,43 @@ export default function Pagination({
         }
     };
 
+    const onClick = (index: number) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', index.toString());
+        router.push(`${pathname}?${params.toString()}`)
+
+    };
+
     return (
-        <div className="flex items-center justify-between border-gray-200 bg-white px-4 py-2 sm:px-6">
-            <div className="flex flex-1 justify-between sm:hidden">
-                <button onClick={handleClickPrevious}
-                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-                >
-                    Previous
-                </button>
-                <button onClick={handleClickNext}
-                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-                >
-                    Next
-                </button>
-            </div>
+        <div className="flex items-center justify-center px-4 py-3 sm:px-6">
 
-            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                <div>
-                    <p className="text-sm text-gray-700">
-                        <span className="font-medium">{properties_per_page * (paginationCurrent_page - 1) + 1}件</span>から<span className="font-medium">{properties_per_page * (paginationCurrent_page - 1) + properties_per_page}件</span>の物件表示 (合計<span className="font-medium">{total}</span>件)
-                    </p>
-                </div>
-                <div>
-                    <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                        <button onClick={handleClickPrevious}
-                            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        >
-                            <MdOutlineFirstPage className="h-5 w-5" aria-hidden="true" />
-                        </button>
+            <div>
+                <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm  bg-white" aria-label="Pagination">
+                    <button onClick={handleClickPrevious}
+                        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    >
+                        <span className="sr-only">Previous</span>
+                        <MdOutlineFirstPage className="h-5 w-5" aria-hidden="true" />
+                    </button>
 
-                        <button onClick={handleClickNext}
-                            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        >
-                            <MdOutlineLastPage className="h-5 w-5" aria-hidden="true" />
-                        </button>
-                    </nav>
-                </div>
+                    <div>
+                        {range(1, num_pages).map((number, index) => (
+                            <button
+                                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                                onClick={() => onClick(number)}
+                            >
+                                {number}
+                            </button>
+                        ))}
+                    </div>
+
+                    <button onClick={handleClickNext}
+                        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    >
+                        <span className="sr-only">Next</span>
+                        <MdOutlineLastPage className="h-5 w-5" aria-hidden="true" />
+                    </button>
+                </nav>
             </div>
         </div>
     );
