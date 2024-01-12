74c7c771889445437e3b29e8c2e7590b6062a493
小針純 <kobarijun@kobarijunnoMBP.hitronhub.home>
2024-01-12 11:42:48 -0800

    easy fix

--- client/app/components/properties/Search.tsx
+++ client/app/components/properties/Search.tsx
@@ -3,11 +3,10 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { BiSearch } from "react-icons/bi";
-import Loading from "@/app/loading";
+
 export default function Search({ placeholder }: { placeholder: string }) {
 
-  const [loading, setLoading] = useState(false);
-    //変数の状態を保持しておく
+  //変数の状態を保持しておく
   const [search_query, setQuery] = useState("");
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -22,10 +21,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
     setQuery(e.target.value)
   }
 
-  const getSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+  const getSearch = (e: React.FormEvent<HTMLFormElement>) => {
 
     e.preventDefault(); // フォームのデフォルトの送信を防止
-    setLoading(true);
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (search_query) {
@@ -33,35 +31,25 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('search_query');
     }
-    try {
-      await router.replace(`${pathname}?${params.toString()}`);
-    } finally {
-      setLoading(false);
-    }
+    router.replace(`${pathname}?${params.toString()}`)
   }
 
   return (
-    <>
-      {loading && (
-        // ローディング中の表示をここに記述
-        <Loading />
-      )}
-      <form onSubmit={getSearch}>
-        <div className="relative w-full">
-          <input
-            type="search"
-            id="default-search"
-            className="block w-full p-3 ps-5 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-            placeholder={placeholder}
-            onChange={updateSearch}
-            value={search_query}
-          />
-          <button type="submit" className="absolute p-2 end-2 bottom-1.5 rounded-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 cursor-pointer">
-            <BiSearch size={18} />
-          </button>
-        </div>
-      </form>
-    </>
+  <form onSubmit={getSearch}>
+      <div className="relative w-full">
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-3 ps-5 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder={placeholder}
+          onChange={updateSearch}
+          value={search_query}
+        />
+        <button type="submit" className="absolute p-2 end-2 bottom-1.5 rounded-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 cursor-pointer">
+          <BiSearch size={18} />
+        </button>
+      </div>
+    </form>
   );
 };
 
