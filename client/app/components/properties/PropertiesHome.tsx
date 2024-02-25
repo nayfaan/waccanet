728e3aacfe6decb03a0c82b0b86085bf9a6b6364
小針純 <kobarijun@kobarijunnoMBP.hitronhub.home>
2024-02-25 00:56:52 -0800

    suspence library changed to work.

--- client/app/components/properties/PropertiesHome.tsx
+++ client/app/components/properties/PropertiesHome.tsx
@@ -1,14 +1,9 @@
-import PropertiesList from "./PropertiesList";
-import Loading from "@/app/loading";
+import React from "react";
 import { Suspense } from "react";
 import Sidebar from "../sidebar/Sidebar";
+import Loading from "@/app/loading";
+import PropertiesMain from "@/app/components/properties/PropertiesMain";
 import Footer from "../footer/Footer";
-
-import { PaginationProperties } from "@/app/types/types";
-import React from "react";
-import { fetchPropertyListPagination } from "../../lib/data";
-import PaginationTop from "../pagination/PaginationTop";
-import Pagination from "../pagination/Pagination";
 
 interface PropertiesHomeProps {
   search_query: string;
@@ -27,16 +22,6 @@ const PropertiesHome: React.FC<PropertiesHomeProps> = async ({
   reference,
   page,
 }) => {
-  const paginationPropertiesData: PaginationProperties =
-    await fetchPropertyListPagination(
-      search_query,
-      price_from,
-      price_to,
-      areas,
-      reference,
-      page
-    );
-
   return (
     <main className="bg-gray-50 relative overflow-y-auto md:ml-72 min-h-screen flex flex-col justify-between pt-14">
       <Sidebar />
@@ -45,29 +30,14 @@ const PropertiesHome: React.FC<PropertiesHomeProps> = async ({
         key={`${search_query}-${price_from}-${price_to}-${areas}-${reference}-${page}`}
         fallback={<Loading />}
       >
-        {paginationPropertiesData.results.length > 0 && (
-          // 物件データがない時は表示しない
-          <div className="p-1 sm:px-6 min-w-screen">
-            <PaginationTop
-              total={paginationPropertiesData.total}
-              current_page={paginationPropertiesData.current_page}
-              properties_per_page={paginationPropertiesData.properties_per_page}
-            />
-          </div>
-        )}
-        {/* <div className="flex flex-col justify-between h-full"> */}
-
-        <PropertiesList properties={paginationPropertiesData.results} />
-        {/* </div> */}
-        {paginationPropertiesData.results.length > 0 && (
-          // 物件データがない時は表示しない
-          <Pagination
-            total={paginationPropertiesData.total}
-            current_page={paginationPropertiesData.current_page}
-            properties_per_page={paginationPropertiesData.properties_per_page}
-            num_pages={paginationPropertiesData.num_pages}
-          />
-        )}
+        <PropertiesMain
+          search_query={search_query}
+          price_from={price_from}
+          price_to={price_to}
+          areas={areas}
+          reference={reference}
+          page={page}
+        />
       </Suspense>
 
       <Footer />
