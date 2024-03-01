e449ed1a0d0a923c08c5914ff76396e01f0f2b04
小針純 <kobarijun@kobarijunnoMBP.hitronhub.home>
2024-02-29 23:52:15 -0800

    easy fix

--- backend/api-server/propertyhub/models.py
+++ backend/api-server/propertyhub/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator
 
 
 class Owner(models.Model):
-    user_id = models.AutoField(primary_key=True,validators=[MinValueValidator(1)])
+    user_id = models.AutoField(primary_key=True,validators=[MinValueValidator(1)],null=True)
     user_name = models.CharField(max_length=50)
     password = models.CharField(max_length=30,null=True)
     email = models.EmailField(max_length = 254,null=True)
