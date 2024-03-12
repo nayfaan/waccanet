360b2da32389b8076edff4bb34ea5bb01ce83134
Manami <vaattii1996@gmail.com>
2024-03-12 15:01:14 -0700

    modify contact page

--- client/app/contact-us/ContactForm.tsx
+++ client/app/contact-us/ContactForm.tsx
@@ -1,126 +1,60 @@
-"use client";
+// "use client";
 
-import Button from "@/app/components/Button";
-import { createContact } from "../lib/action";
+// import Button from "@/app/components/Button";
+// import Input from "../components/inputs/Input";
+// import { createContact } from "../lib/action";
+// import Dropdown from "../components/inputs/Dropdown";
+// import { useState } from "react";
+// import { ContactData } from "../types/types";
 
-const ContactForm = () => {
-  return (
-    <div className="w-full md:w-1/2 xl:w-1/3 flex flex-col mt-5 border rounded-xl p-4 sm:p-6 lg:p-8">
-      <form action={createContact}>
-        <div className="grid gap-4 lg:gap-6">
-          {/* <!-- Grid --> */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
-            <div>
-              <label
-                htmlFor="first-name"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                First name
-              </label>
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 p-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                required
-              />
-            </div>
+// const ContactForm = () => {
+//   const [contactData, setContactData] = useState<ContactData>({
+//     pub_date: new Date(),
+//     first_name: "",
+//     last_name: "",
+//     email_address: "",
+//     contact_type: "",
+//     detail: "",
+//   });
 
-            <div>
-              <label
-                htmlFor="last-name"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Last name
-              </label>
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 p-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                required
-              />
-            </div>
-          </div>
-          {/* <!-- End Grid --> */}
+//   const handleInputChange = () => {};
 
-          {/* <!-- Grid --> */}
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 p-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                required
-              />
-            </div>
-          </div>
+//   return (
+//     <div className="w-full md:w-1/2 xl:w-1/3 flex flex-col mt-5 border rounded-xl p-4 sm:p-6 lg:p-8">
+//       <form action={createContact}>
+//         <div className="grid gap-4 lg:gap-6">
+//           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+//             <Input id="last-name" label="名字" required />
+//             <Input id="first-name" label="名前" required />
+//           </div>
+//           <Input id="email" label="メールアドレス" required />
+//           <Dropdown
+//             id="type"
+//             label="カテゴリ"
+//             value={contactData.contact_type}
+//             onChange={() => {}}
+//             items={[
+//               "質問",
+//               "バグ報告",
+//               "案件依頼",
+//               "物件の追加・削除",
+//               "追加機能の提案",
+//             ]}
+//           />
+//           <Input id="content" label="お問い合わせ内容" textarea required />
+//         </div>
+//         <div className="mt-6 flex flex-col justify-center gap-1">
+//           <button
+//             type="submit"
+//             className="rounded-md bg-blue-500 hover:bg-blue-600 px-5 py-2 text-sm font-medium text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+//           >
+//             送信
+//           </button>
+//           <Button label="戻る" small actionType="back" color="gray" />
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
-            <div>
-              <label
-                htmlFor="type"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                カテゴリ
-              </label>
-              <select
-                id="type"
-                name="type"
-                autoComplete="type-name"
-                className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                required
-              >
-                <option>質問</option>
-                <option>バグ報告</option>
-                <option>案件依頼</option>
-                <option>追加機能の提案</option>
-              </select>
-            </div>
-
-            <div className="col-span-full">
-              <label
-                htmlFor="about"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                内容
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={5}
-                  className="block w-full rounded-md border-0 p-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
-                  required
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="mt-6 flex flex-col justify-center gap-1">
-          <button
-            type="submit"
-            className="rounded-md bg-blue-500 hover:bg-blue-600 px-5 py-2 text-sm font-medium text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
-          >
-            送信
-          </button>
-          <Button label="戻る" small actionType="back" color="gray" />
-        </div>
-      </form>
-    </div>
-  );
-};
-
-export default ContactForm;
+// export default ContactForm;
