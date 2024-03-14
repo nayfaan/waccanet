89b4d93cb5104fd37149f1e5144878e63d451d5f
小針純 <kobarijun@kobarijunnoMBP.hitronhub.home>
2024-03-13 18:45:27 -0700

    can use filter functions

--- client/app/components/properties/PropertiesHome.tsx
+++ client/app/components/properties/PropertiesHome.tsx
@@ -5,43 +5,29 @@ import PropertiesMain from "@/app/components/properties/PropertiesMain";
 import Footer from "../footer/Footer";
 import SidebarProvider from "../sidebar/SidebarProvider";
 
-interface PropertiesHomeProps {
-  search_query: string;
-  price_from: string;
-  price_to: string;
-  areas: string;
-  reference: string;
-  page: number;
-}
+export default function PropertiesHome({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string };
+}) {
+  let query = "";
+  for (const key in searchParams) {
+    if (Object.hasOwnProperty.call(searchParams, key)) {
+      const value = searchParams[key];
+      query += `${key}=${value}&`;
+    }
+  }
+  if (query.slice(-1) == "&") {
+    query = query.slice(0, -1);
+  }
 
-const PropertiesHome: React.FC<PropertiesHomeProps> = async ({
-  search_query,
-  price_from,
-  price_to,
-  areas,
-  reference,
-  page,
-}) => {
   return (
     <main className="bg-gray-50 relative overflow-y-auto md:ml-72 min-h-screen flex flex-col justify-between pt-14">
       <SidebarProvider />
-      <Suspense
-        key={`${search_query}-${price_from}-${price_to}-${areas}-${reference}-${page}`}
-        fallback={<Loading />}
-      >
-        <PropertiesMain
-          search_query={search_query}
-          price_from={price_from}
-          price_to={price_to}
-          areas={areas}
-          reference={reference}
-          page={page}
-        />
+      <Suspense key={`${query}`} fallback={<Loading />}>
+        <PropertiesMain query={query} />
       </Suspense>
-
       <Footer />
     </main>
   );
-};
-
-export default PropertiesHome;
+}
